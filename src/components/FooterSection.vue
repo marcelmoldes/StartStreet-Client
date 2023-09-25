fdcf24b501e552d96b50806aa0b0019211ed7708
Marcel <moldesmarcel41@gmail.com>
2023-09-25 20:06:10 +0200

    new

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+<div class="bg-black">
+<ol>
+    
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+
+    
+
+</ol>
+</div>
+
+
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    
+    <div class=" ">
+    
+<h1 class="text-red-500 border-4 mb-20 ">HOla</h1>
+<h1 class=" text-vuejs">HOla</h1>
+<p class="text-9xl">Hola Caracola</p>
+<p class="text-base italic">Hola Caracola</p>
+<p class="text-xl">Hola Caracola</p>
+<p class="text-lg">Hola Caracola</p>C
+<p class="text-2xl">Hola Caracola</p>
+<p class="text-3xl">Hola Caracola</p>
+
+
+
+
+
+
+
+
+   <h1 class=""></h1>
+
+
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
