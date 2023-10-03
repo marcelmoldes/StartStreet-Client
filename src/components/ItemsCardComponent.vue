13d28a8f1043ab059b416eaabb6b0e0435643ff9
Marcel <moldesmarcel41@gmail.com>
2023-10-03 09:25:19 +0200

    favorites

--- src/components/ItemsCardComponent.vue
+++ src/components/ItemsCardComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-9 text-center lg:w-80 grid-cols-4 w-50 leading-150 py-20">
     <a :href="`/items/${item.slug}`" class="">
-      <img :src="item.image" class="ring ring-white w-96 h-96" />
+      <img :src="item.images[0].url" class="ring ring-white w-96 h-96" />
     
       <h1
         class="font-extrabold underline decoration-1 text-xl p-5 font-mono text-gray-200 items-center align-items text-center m-7 mt-6 rounded-lg h-6"
