ca2fb5e92ad353f65498756c32cc3c64aa46d921
marcelmoldes <moldesmarcel41@gmail.com>
2023-10-31 15:32:22 +0100

    Remove starstreet folder

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -51,45 +51,29 @@
             >
             <div class="mt-2">
               <div
-                class="flex mt-6 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                class="flex mt-6 gap-x-2"
               >
                 <span
-                  class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"
-                  >{{ client.email }}</span
+                  class="flex select-none items-center pl-3 font-semibold sm:text-sm"
+                  >E-mail</span
                 >
                 <input
                   type="text"
-                  name="website"
-                  id="website"
-                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  :value="client.email"
+                  class="block rounded-md shadow-sm ring-gray-300 ring-1 flex-1 border-0 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-black sm:text-sm sm:leading-6"
                 />
               </div>
               <div
-                class="flex mt-5 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                class="flex mt-6 gap-x-2"
               >
                 <span
-                  class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"
-                  >{{ client.client_name }}</span
+                  class="flex select-none items-center pl-3 font-semibold sm:text-sm"
+                  >Name</span
                 >
                 <input
                   type="text"
-                  name="website"
-                  id="website"
-                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                />
-              </div>
-              <div
-                class="flex mt-4 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
-              >
-                <span
-                  class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"
-                  >{{ client.a }}</span
-                >
-                <input
-                  type="text"
-                  name="website"
-                  id="website"
-                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  :value="client.client_name"
+                  class="block rounded-md shadow-sm ring-gray-300 ring-1 flex-1 border-0 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-black sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
--- src/views/OrderDetailsView.vue
+++ src/views/OrderDetailsView.vue
@@ -3,7 +3,6 @@
     :client="client"
     :item="item"
     :order="order"
-
   ></order-details-component>
 </template>
 
@@ -11,27 +10,30 @@
 import OrderDetailsComponent from "@/components/OrderDetailsComponent.vue";
 import axios from "axios";
 export default {
-props:['client','item','order'],
+  props: ["client", "item"],
   components: {
     OrderDetailsComponent,
   },
   data() {
     return {
-      orders: [],
-      
+      order: {},
     };
   },
-
-  async loadData() {
-    let response = await axios.get(
-      "http://localhost:8081/orders/" + this.$route.params.order.id,
-      {
-        headers: {
-          Authorization: this.client ? "Bearer " + this.client.token : null,
-        },
-      }
-    );
-    this.orders = response.data.order;
+  async mounted() {
+    await this.loadData();
+  },
+  methods: {
+    async loadData() {
+      let response = await axios.get(
+        "http://localhost:8081/orders/" + this.$route.params.id,
+        {
+          headers: {
+            Authorization: this.client ? "Bearer " + this.client.token : null,
+          },
+        }
+      );
+      this.order = response.data.order;
+    },
   },
 };
 </script>
